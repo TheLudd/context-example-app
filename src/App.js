8f6c56227654d3bd0d1e4469562d9adbcf6ed035
Ludwig Magnusson <ludwig@mediatool.com>
2020-01-14 15:31:17 +0100

    Implement magnificent app with loading props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,91 @@
 import React from 'react';
 import cx from 'classnames'
-import { curry } from 'yafu'
-import { append, assoc, map } from 'ramda'
+import { append, reject, identical, contains, assoc, map } from 'ramda'
 import './App.css';
 
-const { useState, useEffect } = React
+const {
+  createContext,
+  useContext,
+  useState,
+  useMemo,
+  useEffect
+} = React
+
+const TodoContext = createContext({})
 
 const initialTodos = [
   { _id: 'td0', name: 'Buy milk', done: false },
   { _id: 'td1', name: 'Write book', done: true },
 ]
 
-function fetchTodos (setState) {
-  setTimeout(setState, 1000, initialTodos)
-}
-
-function useTheContext () {
-  const [ state, setState ] = useState([])
-
-  useEffect(() => {
-    fetchTodos(setState)
-  }, [])
+function constructTodoActions (stateModifiers) {
+  const { setList, setLoadingTodos, setIsCreating } = stateModifiers
+  function fetchTodos () {
+    setTimeout(setList, 1000, initialTodos)
+  }
 
   function setItemStatus (id, status) {
+    setLoadingTodos(append(id))
     setTimeout(() => {
-      setState(map((item) => item._id === id ? assoc('done', status, item) : item))
+      setList(map((item) => item._id === id ? assoc('done', status, item) : item))
+      setLoadingTodos(reject(identical(id)))
     }, 1000)
   }
 
   function createItem (name) {
+    setIsCreating(true)
     setTimeout(() => {
-      setState((list) => {
+      setList((list) => {
         return append({ _id: `td${list.length}`, name, done: false }, list)
       })
+      setIsCreating(false)
     }, 1000)
   }
 
-  return { state, setItemStatus, createItem }
+  return {
+    createItem,
+    fetchTodos,
+    setItemStatus,
+  }
+}
+
+function useTodoResource () {
+  const [ list, setList ] = useState([])
+  const [ loadingTodos, setLoadingTodos ] = useState([])
+  const [ isCreating, setIsCreating ] = useState(false)
+  const actions = useMemo(() => {
+    return constructTodoActions({ setList, setLoadingTodos, setIsCreating })
+  }, [ setList, setLoadingTodos, setIsCreating ])
+
+  useEffect(() => {
+    actions.fetchTodos()
+  }, [ actions ])
+
+  return { list, actions, loadingTodos, isCreating }
+}
+
+function useTodoStore () {
+  const {
+    list: todos,
+    loadingTodos,
+    actions: todoActions,
+    isCreating,
+  } = useTodoResource()
+  const state = { resources: { todos }, loadingProps: { loadingTodos }, isCreating }
+
+  return { state, actions: { todoActions } }
 }
 
 function TodoItem (props) {
-  const { todo, onStatusChange } = props
+  const { todo, isLoading, onStatusChange } = props
   const { done } = todo
   const statusClass = cx({
     'is-done': done,
   })
+
+  if (isLoading) {
+    return <div>LOADING</div>
+  }
 
   return (
     <div className="todo">
@@ -56,13 +97,14 @@ function TodoItem (props) {
   )
 }
 
-const createTodoItem = curry((changeStatus, todo) => {
-  return <TodoItem key={ todo._id } todo={ todo } onStatusChange={ changeStatus } />
+function TodoApp () {
+  const { state, actions } = useContext(TodoContext)
+  const { resources, loadingProps, isCreating } = state
+  const { todoActions } = actions
+  const { todos } = resources
+  const { loadingTodos } = loadingProps
+  const { createItem, setItemStatus } = todoActions
 
-})
-
-function App() {
-  const { state: todos, setItemStatus, createItem } = useTheContext()
   const [ name, setName ] = useState('')
 
   function handleSubmit (e) {
@@ -74,11 +116,18 @@ function App() {
     setItemStatus(todo._id, !todo.done)
   }
 
+  useEffect(() => {
+    if (!isCreating) {
+      setName('')
+    }
+  }, [ isCreating ])
+
   return (
     <div className="content">
       <form onSubmit={ handleSubmit } className="todo-form" >
         <label>Add a Todo</label>
         <input
+          disabled={ isCreating }
           type="text"
           value={ name }
           onChange={ (e) => setName(e.target.value) }
@@ -86,10 +135,27 @@ function App() {
         <button type="submit">+ Add</button>
       </form>
       <div className="todos">
-        { map(createTodoItem(changeStatus), todos) }
+        { map((item) => {
+          return (
+            <TodoItem
+              key={ item._id }
+              todo={ item }
+              isLoading={ contains(item._id, loadingTodos) }
+              onStatusChange={ changeStatus }
+            />
+          )
+        }, todos) }
       </div>
     </div>
+  )
+}
+
+function Main() {
+  return (
+    <TodoContext.Provider value={ useTodoStore() } >
+      <TodoApp />
+    </TodoContext.Provider>
   );
 }
 
-export default App;
+export default Main;
